142e33a79e2f1253585696a194ed71e3a09a27db
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-08-12 10:38:16 +1000

    Improve csv script

--- scripts/insta-locations-to-csv.ts
+++ scripts/insta-locations-to-csv.ts
@@ -4,29 +4,37 @@ import meow from 'meow';
 import Path from 'node:path';
 import { deferral } from '@danoaky/js-utils/disposables';
 import * as Z from 'zod';
+import { marshall } from '@aws-sdk/util-dynamodb';
 
 const nthUrlPart = (str: string, n: number) => str.split('/')[n];
+const escapedQuoteRe = /"/g;
 
 const locationSchema = Z.object({
 	url: Z.string().transform((str) => nthUrlPart(str, 0)),
-	name: Z.string().transform((str) =>
-		str
+	name: Z.string().transform((str) => {
+		let result = str
 			.replace(/[A-Z][^ ]/g, (m) => ' ' + m)
 			.trim()
 			.replace(/ {2,}/g, ' ')
-	),
+			.replace(/\t|\\t/g, '');
+		const numOfDoubleQuotes = result.match(escapedQuoteRe)?.length ?? 0;
+		if (numOfDoubleQuotes % 2 === 1) {
+			return result.replaceAll('"', `'`); // Swap these odd quotes to single apostrophes
+		}
+		return result.replaceAll('"', `"""`); // DynamoDB needs " to be escaped with """
+	}),
 	parent_url: Z.string()
 		.nullish()
 		.transform((str) => (str ? nthUrlPart(str, 0) : undefined))
 });
 
-export async function exportAsDynamoDBJson(db: Database, outputDir: string) {
+export async function exportAsDynamoDBJson(db: Database, outputDir: string, delimiter = '\t') {
 	await using defer = deferral();
 	const filePath = Path.join(outputDir, `locations.csv`);
 	const file = Bun.file(filePath);
 	if (await file.exists()) await file.delete();
 	const writer = file.writer();
-	writer.write('url,name,parent_url\n');
+	writer.write(`url${delimiter}name${delimiter}parent_url\n`);
 	defer(async () => {
 		await writer.end();
 	});
@@ -37,7 +45,7 @@ export async function exportAsDynamoDBJson(db: Database, outputDir: string) {
 	for await (const row of db.query(`SELECT * FROM locations`)) {
 		const { data, error } = locationSchema.safeParse(row);
 		if (error) throw error;
-		writer.write([data.url, data.name, data.parent_url].join(',') + '\n'); // TODO: add parent_url
+		writer.write([data.url, data.name, data.parent_url].join(delimiter) + '\n');
 		await writer.flush();
 		i++;
 		if (i % 10000 === 0) logRow();
