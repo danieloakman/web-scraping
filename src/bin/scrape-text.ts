abbab1a05a15e4e5960e572b133c5dd2f8da0fd2
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2026-02-15 04:09:59 +1100

    Some work towards scraping

--- src/bin/scrape-text.ts
+++ src/bin/scrape-text.ts
@@ -1,51 +1,78 @@
 #! bun
-import { extractText } from "@/functions/webpage/utils";
-import { launchBrowser } from "@/utils/browser";
-import meow from "meow";
-import { writeFileSync } from "fs";
+import { extractText, getPageText } from '@/functions/webpage/utils';
+import { launchBrowser, newPage } from '@/utils/browser';
+import meow from 'meow';
+import { writeFileSync } from 'fs';
+import { constant } from '@danoaky/js-utils';
+import { sleep } from 'bun';
 
 if (import.meta.main) {
-  const { input: urls, flags: { output, help }, showHelp } = meow(`Scrape the text from webpages.
+	const {
+		input: urls,
+		flags: { output, help },
+		showHelp
+	} = meow(
+		`Scrape the text from webpages.
 
   Usage:
     $ scrape-text <url>...
 
   Examples:
     $ scrape-text https://www.google.com
-    $ scrape-text https://www.google.com https://www.bing.com`, {
-    importMeta: import.meta,
-    flags: {
-      help: {
-        type: 'boolean',
-        default: false,
-        shortFlag: 'h',
-        description: 'Show help message and exit.',
-      },
-      output: {
-        type: 'string',
-        default: '',
-        shortFlag: 'o',
-        description: 'The output json file path to write to. If not provided, the text will be written to the console.',
-      }
-    },
-  });
-  if (help)
-    showHelp(0);
-  if (!urls.length) {
-    console.error('No URLs provided');
-    process.exit(1);
-  }
+    $ scrape-text https://www.google.com https://www.bing.com`,
+		{
+			importMeta: import.meta,
+			flags: {
+				help: {
+					type: 'boolean',
+					default: false,
+					shortFlag: 'h',
+					description: 'Show help message and exit.'
+				},
+				output: {
+					type: 'string',
+					default: '',
+					shortFlag: 'o',
+					description:
+						'The output json file path to write to. If not provided, the text will be written to the console.'
+				}
+			}
+		}
+	);
+	if (help) showHelp(0);
+	if (!urls.length) {
+		console.error('No URLs provided');
+		process.exit(1);
+	}
 
-  await using browser = await launchBrowser();
-  const result: Record<string, string> = {};
-  for (const url of urls) {
-    console.log(`Scraping ${url} ...`);
-    const text = await extractText(browser, url);
-    if (output)
-      result[url] = text;
-    else
-      console.log(text + '\n\n');
-  }
-  if (output)
-    writeFileSync(output, JSON.stringify(result, null, 2));
+	await using browser = await launchBrowser();
+	const result: Record<string, string> = {};
+	for (const url of urls) {
+		console.log(`Scraping ${url} ...`);
+		await using page = await newPage(browser);
+		await page.goto(url, { timeout: 10000 });
+		// Check for a captcha that contains the text "confirm you are human"
+		const captcha = await page
+			.getByText('confirm you are human')
+			.first();
+		if (await captcha.count().catch(constant(0))) {
+			console.log('Captcha detected');
+			await captcha.click();
+		}
+		while (true) {
+			await sleep(5000);
+			const captcha = page.getByText('confirm you are human').first();
+			if (await captcha.count().catch(constant(0))) {
+				console.log('Captcha detected');
+				continue;
+			} else {
+				console.log('No captcha detected');
+				break;
+			}
+		}
+		const text = await getPageText(page);
+		if (output) result[url] = text;
+		else console.log(text + '\n\n');
+	}
+	if (output) writeFileSync(output, JSON.stringify(result, null, 2));
 }
