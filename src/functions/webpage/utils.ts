15e1b302a425cc3a24c0a4090bbe748904e2e5f4
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2026-02-15 04:05:12 +1100

    Add getPageText

--- src/functions/webpage/utils.ts
+++ src/functions/webpage/utils.ts
@@ -1,10 +1,14 @@
 import { newPage } from "@/utils/browser";
-import type { Browser } from "playwright-core";
+import type { Browser, Page } from "playwright-core";
 
 export async function extractText(browser: Browser, url: string) {
   await using page = await newPage(browser);
   await page.goto(url, { timeout: 10000 });
-  const content = await page.evaluate(
+  return getPageText(page);
+}
+
+export function getPageText(page: Page) {
+  return page.evaluate(
     () => {
       const text = (document.querySelector('main') ?? document.querySelector('body'))?.innerText;
       if (!text) throw new Error('Could not find main element to get text content from');
@@ -14,6 +18,4 @@ export async function extractText(browser: Browser, url: string) {
       timeout: 10000
     }
   );
-  return content;
 }
-
